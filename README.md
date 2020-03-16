2744d501fe007031b8a5e8d9f9c817ea3894c65d
xiaolei19960829 <1974392131@qq.com>
2020-03-16 09:04:45 +0800

    add index

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## 资料
 https://spring.io/guides/gs/serving-web-content/
-
+https://elasticsearch.cn/
+https://v3.bootcss.com/css/#grid-options
 ## 工具
 https://git-scm.com/download
--- src/main/java/life/xl/community/controller/IndexController.java
+++ src/main/java/life/xl/community/controller/IndexController.java
@@ -3,7 +3,6 @@ package life.xl.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -11,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Date Created in 22:03 2020/3/15
  */
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-    model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
